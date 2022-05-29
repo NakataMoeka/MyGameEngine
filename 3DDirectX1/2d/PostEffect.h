bac7fbe9776108551c614a9b553ac8e054003714
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-05-29 21:43:51 +0900

    途中

--- 3DDirectX1/2d/PostEffect.h
+++ 3DDirectX1/2d/PostEffect.h
@@ -2,9 +2,21 @@
 #include "Sprite.h"
 class PostEffect :public Sprite
 {
+private:
+
+	template<class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
 public:
 	PostEffect();
 
+	void Initialize();
+
 	void Draw(ID3D12GraphicsCommandList* cmdList);
+
+private:
+	//テクスチャバッファ
+	ComPtr<ID3D12Resource> texBuff;
+	//デスクリプタヒープ
+	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
+
 };
 
