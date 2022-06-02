522473e6a5add3136745e33d462587e6dfb670eb
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-06-03 01:11:36 +0900

    1_5完了(1_4の実行結果が違う理由は分からなかったけど)

--- 3DDirectX1/2d/PostEffect.h
+++ 3DDirectX1/2d/PostEffect.h
@@ -1,34 +1,60 @@
 #pragma once
 #include "Sprite.h"
-class PostEffect :public Sprite
+#include <DirectXMath.h>
+class PostEffect : public Sprite
 {
-private:
-
-	template<class T>using ComPtr = Microsoft::WRL::ComPtr<T>;
-
+	// Microsoft::WRL::を省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::を省略
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
 public:
-	static const float clearColor[4];
+
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
 	PostEffect();
 
+	/// <summary>
+	/// 初期化
+	/// </summary>
 	void Initialize();
 
-	void Draw(ID3D12GraphicsCommandList* cmdList);
 
+
+	/// <summary>
+	/// シーン描画前処理
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
 	void PreDrawScene(ID3D12GraphicsCommandList* cmdList);
 
+	/// <summary>
+	/// シーン描画後処理
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
 	void PostDrawScene(ID3D12GraphicsCommandList* cmdList);
 
-private:
+	/// <summary>
+	/// 描画コマンドの発行
+	/// </summary>
+	/// <param name="cmdList">コマンドリスト</param>
+	void Draw(ID3D12GraphicsCommandList* cmdList);
+
 	//テクスチャバッファ
 	ComPtr<ID3D12Resource> texBuff;
-	//デスクリプタヒープ
+	//SRV用デスクリプタヒープ
 	ComPtr<ID3D12DescriptorHeap> descHeapSRV;
-	// 深度バッファ
+	//深度バッファ
 	ComPtr<ID3D12Resource> depthBuff;
-	// RTV用デスクリプタヒープ
+	//RTV用デスクリプタヒープ
 	ComPtr<ID3D12DescriptorHeap> descHeapRTV;
-	// DSV用デスクリプタヒープ
+	//DSV用デスクリプタヒープ
 	ComPtr<ID3D12DescriptorHeap> descHeapDSV;
 
-};
 
+	//画面クリアカラー
+	static const float clearColor[4];
+
+};
