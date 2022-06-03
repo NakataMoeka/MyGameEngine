cf3c0f7f91a4a506dd550cf4889cc0485c669e90
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-06-03 13:07:45 +0900

    ぼかし

--- 3DDirectX1/Resources/shaders/PostEffectTestPS.hlsl
+++ 3DDirectX1/Resources/shaders/PostEffectTestPS.hlsl
@@ -5,6 +5,23 @@ SamplerState smp : register(s0);		//0番スロットに設定されたサンプラー
 
 float4 main(VSOutput input) : SV_TARGET
 {
-	float4 texcolor = tex.Sample(smp,input.uv);
+	//ぼかし
+	float oU = 2.5 / 1280;
+	float oV = 2.5 / 720;
+	float4 texcolor = tex.Sample(smp, input.uv);
+	
+	texcolor += tex.Sample(smp, input.uv + float2(oU, 0.0f));
+	texcolor += tex.Sample(smp, input.uv + float2(-oU, 0.0f));
+	texcolor += tex.Sample(smp, input.uv + float2(0.0f, oV));
+	texcolor += tex.Sample(smp, input.uv + float2(0.0f, -oV));
+	texcolor += tex.Sample(smp, input.uv + float2(0.0f, 0.0f));
+	texcolor += tex.Sample(smp, input.uv + float2(oU, oV));
+	texcolor += tex.Sample(smp, input.uv + float2(oU, -oV));
+	texcolor += tex.Sample(smp, input.uv + float2(-oU, oV));
+	texcolor += tex.Sample(smp, input.uv + float2(-oU, -oV));
+	
+	texcolor /= 9.0f;
+	//ここまで
+	
 	return float4(texcolor.rgb, 1);
 }
