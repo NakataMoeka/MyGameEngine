0a09d6854344c965cd251c4808738741feec152c
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-06-22 15:57:04 +0900

    イージングクラス作り始めた

---
+++ 3DDirectX1/Ease.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<DirectXMath.h>
+class Ease
+{
+private:
+	// Microsoft::WRL::‚ğÈ—ª
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+	// DirectX::‚ğÈ—ª
+	using XMFLOAT2 = DirectX::XMFLOAT2;
+	using XMFLOAT3 = DirectX::XMFLOAT3;
+	using XMFLOAT4 = DirectX::XMFLOAT4;
+	using XMMATRIX = DirectX::XMMATRIX;
+public:
+
+};
+
