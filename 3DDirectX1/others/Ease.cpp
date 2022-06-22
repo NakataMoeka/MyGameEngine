f9c4e5c2f6b01d8d9fb12cae254d48db54c98222
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-06-23 01:38:53 +0900

    イージング関数追加

--- 3DDirectX1/others/Ease.cpp
+++ 3DDirectX1/others/Ease.cpp
@@ -1,1 +1,49 @@
 #include "Ease.h"
+using namespace DirectX;
+
+//t‚ÍŠÔ‚Å‚Í‚È‚­ŠÔ‚ÌŠ„‡
+
+const XMFLOAT2 Eas::lerp(const XMFLOAT2& start, const XMFLOAT2& end, const float t)
+{
+	float x, y = 0;
+
+	x = start.x * (1.0f - t) + end.x * t;
+	y = start.y * (1.0f - t) + end.y * t;
+
+
+	return XMFLOAT2({ x, y });
+}
+
+
+const  XMFLOAT3 Eas::lerp(const XMFLOAT3& start, const XMFLOAT3& end, const float t)
+{
+	float x, y, z = 0;
+
+	x = start.x * (1.0f - t) + end.x * t;
+	y = start.y * (1.0f - t) + end.y * t;
+	z = start.z * (1.0f - t) + end.z * t;
+
+	return XMFLOAT3({ x, y, z });
+}
+const XMFLOAT2 Eas::easeIn(const XMFLOAT2& start, const XMFLOAT2& end, const float t)
+{
+	float x, y = 0;
+
+	//t*t‚ğ
+
+	x = start.x * (1.0f - t * t) + end.x * (t * t);
+	y = start.y * (1.0f - t * t) + end.y * (t * t);
+	return XMFLOAT2({ x, y });
+}
+const XMFLOAT3 Eas::easeIn(const XMFLOAT3& start, const XMFLOAT3& end, const float t)
+{
+	float x, y, z = 0;
+
+	//t*t‚ğ
+
+	x = start.x * (1.0f - t * t) + end.x * (t * t);
+	y = start.y * (1.0f - t * t) + end.y * (t * t);
+	z = start.z * (1.0f - t * t) + end.z * (t * t);
+
+	return XMFLOAT3({ x, y, z });
+}
--- 3DDirectX1/others/Ease.h
+++ 3DDirectX1/others/Ease.h
@@ -1,7 +1,13 @@
 #pragma once
+#include <Windows.h>
+#include <wrl.h>
+#include <d3d12.h>
 #include <DirectXMath.h>
-class Ease
+#include <d3dx12.h>
+
+class Eas
 {
+
 private:
 	// Microsoft::WRL::Çè»ó™
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
@@ -10,7 +16,13 @@ private:
 	using XMFLOAT3 = DirectX::XMFLOAT3;
 	using XMFLOAT4 = DirectX::XMFLOAT4;
 	using XMMATRIX = DirectX::XMMATRIX;
-
 public:
+	static const XMFLOAT2 lerp(const XMFLOAT2& start, const XMFLOAT2& end, const float t);
+	static const XMFLOAT3 lerp(const XMFLOAT3& start, const XMFLOAT3& end, const float t);
+
+	static const XMFLOAT2 easeIn(const XMFLOAT2& start, const XMFLOAT2& end, const float t);
+	static const XMFLOAT3 easeIn(const XMFLOAT3& start, const XMFLOAT3& end, const float t);
+
 };
+
 
