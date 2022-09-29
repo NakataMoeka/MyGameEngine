c434a44153adc9f90846d04fab7a380af0d0bf00
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-09-29 18:37:54 +0900

    csvファイル読み込み関数の追加

---
+++ 3DDirectX1/others/CSV.h
@@ -0,0 +1,7 @@
+#pragma once
+#define MAP_WIDTH (40) //‰¡‚ÌÅ‘å–‡”
+#define MAP_HEIGHT (20)  //c‚ÌÅ‘å–‡”
+
+int LoadCSV(int map[MAP_HEIGHT][MAP_WIDTH], const char* FilePath, int LoadStopNumber = -1);
+
+
