fcc202a34baeb3accc328da430522b6418300016
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-11-15 15:18:05 +0900

    縦横のサイズ大小問わずcsv読み込みできない

--- 3DDirectX1/others/CSV.h
+++ 3DDirectX1/others/CSV.h
@@ -1,5 +1,5 @@
 #pragma once
-#define MAP_WIDTH (26) //‰¡‚ÌÅ‘å–‡”
+#define MAP_WIDTH (10) //‰¡‚ÌÅ‘å–‡”
 #define MAP_HEIGHT (10)  //c‚ÌÅ‘å–‡”
 
 int LoadCSV(int map[MAP_HEIGHT][MAP_WIDTH], const char* FilePath, int LoadStopNumber = -1);
