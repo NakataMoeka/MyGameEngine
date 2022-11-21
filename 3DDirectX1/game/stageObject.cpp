4a121a9e1980da9fb0b9b3f1225cd4c7fa6b9742
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2022-11-21 19:18:20 +0900

    地面のサイズいじった

--- 3DDirectX1/game/stageObject.cpp
+++ 3DDirectX1/game/stageObject.cpp
@@ -21,36 +21,20 @@ void StageObject::Initialize()
 	Home = TouchableObject::Create(modelHome);
 	Home->CreateGraphicsPipeline(L"Resources/shaders/OBJPS.hlsl", L"Resources/shaders/OBJVS.hlsl");
 
-	//for (int i = 0; i < 2; i++) {
-	//	slope[i] = TouchableObject::Create(modelSlope);
-	//	slope[i]->CreateGraphicsPipeline(L"Resources/shaders/OBJPS.hlsl", L"Resources/shaders/OBJVS.hlsl");
-	//}
+
 }
 
 void StageObject::Init()
 {
-	position[0] = { 0,0,10 };
-	position[1] = { 0,0,-50 };
-	size = { 5,5,5 };
-	rota = { 0,0,0 };
 
 }
 
 void StageObject::Update()
 {
-	/*for (int i = 0; i < 2; i++) {
-		slope[i]->Quaternion();
-		slope[i]->SetScale(size);
-		slope[i]->SetRotation(rota);
-		slope[i]->SetPosition(position[i]);
-		slope[i]->Update();
-	}*/
-	//Block->Quaternion();
-	//Block->SetScale({ 5,70,5 });
-	//Block->SetRotation({0,0,0,0});
+
 	Kotatu->SetPosition({ 0,38,-80 });
 	Kotatu->Update();
-	Ground->SetScale({ 4,4,4 });
+	Ground->SetScale({ 6,6,6 });
 	Ground->SetPosition({ 0,0,0 });
 	Ground->SetRotation({ 0,0,0 });
 	Ground->Quaternion();
@@ -60,9 +44,7 @@ void StageObject::Update()
 
 void StageObject::Draw()
 {
-	/*for (int i = 0; i < 2; i++) {
-		slope[i]->Draw();
-	}*/
+
 	Kotatu->Draw();
 	Ground->Draw();
 	Home->Draw();
