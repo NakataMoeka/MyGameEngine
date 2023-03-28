2f4fe20444e459cbf0a47c5dfa963ef8d20cec5a
Nakata_Moeka <k020g1155@g.neec.ac.jp>
2023-03-29 01:31:16 +0900

    ハーフランバートテスト

--- 3DDirectX1/Resources/shaders/halfPS.hlsl
+++ 3DDirectX1/Resources/shaders/halfPS.hlsl
@@ -6,32 +6,47 @@ float4 main(VSOutput input) : SV_TARGET
 
 	// テクスチャマッピング
 	float4 texcolor = tex.Sample(smp, input.uv);
-	// 光沢度
-	const float shininess = 4.0f;
-	// 頂点から視点への方向ベクトル
-	float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+	//// 光沢度
+	//const float shininess = 4.0f;
+	//// 頂点から視点への方向ベクトル
+	//float3 eyedir = normalize(cameraPos - input.worldpos.xyz);
+	//float3 P = input.svpos.xyz;
+	//float3 N = normalize(input.normal);
+	////　Ambient Color
+	//float3 ambient = m_ambient;
+	//float4 shadecolor = float4(ambientColor * ambient, m_alpha);
+	//for (int i = 0; i < DIRLIGHT_NUM; i++) {
+	//	if (dirLights[i].active) {
+	//		// ライトに向かうベクトルと法線の内積
+	//		float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
+	//		// 反射光ベクトル
+	//		float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
+	//		// 拡散反射光
+	//		float3 diffuse = dotlightnormal * m_diffuse;
+	//		// 鏡面反射光
+	//		float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
+
+	//		// 全て加算する
+	//		shadecolor.rgb += (diffuse + specular) * dirLights[i].lightcolor;
+	//	}
+	//}
+
+	//// シェーディングによる色で描画
+	//return (shadecolor * texcolor) * color;
 	float3 P = input.svpos.xyz;
 	float3 N = normalize(input.normal);
+	float Ka = 0.5;
+
 	//　Ambient Color
-	float3 ambient = m_ambient;
-	float4 shadecolor = float4(ambientColor * ambient, m_alpha);
+	float3 ambient = m_ambient * Ka;
+	float3 diffuse = float3(0, 0, 0);
+	//　Diffuse Color
 	for (int i = 0; i < DIRLIGHT_NUM; i++) {
 		if (dirLights[i].active) {
-			// ライトに向かうベクトルと法線の内積
-			float3 dotlightnormal = dot(dirLights[i].lightv, input.normal);
-			// 反射光ベクトル
-			float3 reflect = normalize(-dirLights[i].lightv + 2 * dotlightnormal * input.normal);
-			// 拡散反射光
-			float3 diffuse = dotlightnormal * m_diffuse;
-			// 鏡面反射光
-			float3 specular = pow(saturate(dot(reflect, eyedir)), shininess) * m_specular;
-
-			// 全て加算する
-			shadecolor.rgb += (diffuse + specular) * dirLights[i].lightcolor;
+			float3 L = normalize(float3(0, 0, 1) - P);	//　ローカル座標系のでのライトベクトル
+			float diffuseLight = max(dot(L, N), 0) * 0.5 + 0.5;
+			diffuse = dirLights[i].lightcolor * (diffuseLight * diffuseLight);
 		}
 	}
-
-	// シェーディングによる色で描画
-	return (shadecolor * texcolor) * color;
-
+	return float4(ambient + diffuse, 1.0f) * texcolor;
 }
--- 3DDirectX1/game/player.cpp
+++ 3DDirectX1/game/player.cpp
@@ -27,7 +27,7 @@ void Player::Initialize()
 	model2 = Model::Create("bullet", true);
 	SphereObj = Object3d::Create(model2);
 	//Createの後に書かないとclient.hのInternalRelease()でエラーが起こる//Createの後に書かないとclient.hのInternalRelease()でエラーが起こる
-	SphereObj->CreateGraphicsPipeline(L"Resources/shaders/OBJPS.hlsl", L"Resources/shaders/OBJVS.hlsl");
+	SphereObj->CreateGraphicsPipeline(L"Resources/shaders/halfPS.hlsl", L"Resources/shaders/OBJVS.hlsl");
 	Sprite::LoadTexture(2, L"Resources/dash.png");
 	Sprite::LoadTexture(3, L"Resources/UI/sizeUI.png");
 	Sprite::LoadTexture(4, L"Resources/UI/chikyuu.png");
